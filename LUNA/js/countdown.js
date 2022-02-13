bebdfd2c72aebe7989119baf199dc252eb58f98d
DiegOliveros <diegoo.ivan@gmail.com>
2022-02-13 16:36:36 -0500

    2022 - 02 -13

---
+++ LUNA/js/countdown.js
@@ -0,0 +1,120 @@
+const getTime = dateTo => {
+    let now = new Date(),
+        time = (new Date(dateTo) - now + 1000) / 1000,
+        seconds = ('0' + Math.floor(time % 60)).slice(-2),
+        minutes = ('0' + Math.floor(time / 60 % 60)).slice(-2),
+        hours = ('0' + Math.floor(time / 3600 % 24)).slice(-2),
+        days = Math.floor(time / (3600 * 24));
+ 
+    return {
+        seconds,
+        minutes,
+        hours,
+        days,
+        time
+    }
+};
+ 
+const countdown = (dateTo, element) => {
+    const item = document.getElementById(element);
+ 
+    const timerUpdate = setInterval( () => {
+        let currenTime = getTime(dateTo);
+        item.innerHTML = `
+            <div class="row">
+                <div class="col-lg-2">
+                    <div class="countdown-container">
+                        <div class="number">
+                            ${currenTime.days}
+                        </div>
+                        <div class="concept">
+                            Días
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-2">
+                    <div class="countdown-container">
+                        <div class="number">
+                            ${currenTime.hours}
+                        </div>
+                        <div class="concept">
+                            Horas
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-2">
+                    <div class="countdown-container">
+                        <div class="number">
+                            ${currenTime.minutes}
+                        </div>
+                        <div class="concept">
+                            Minutos
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-2">
+                    <div class="countdown-container">
+                        <div class="number">
+                            ${currenTime.seconds}
+                        </div>
+                        <div class="concept">
+                            Segundos
+                        </div>
+                    </div>
+                </div>
+            </div>`;
+ 
+        if (currenTime.time <= 1) {
+            clearInterval(timerUpdate);
+            alert('Fin de la cuenta '+ element);
+        }
+ 
+    }, 1000);
+};
+
+
+
+//===
+// VARIABLES
+//===
+const DATE_TARGET = new Date('04/04/2020 0:01 AM');
+// DOM for render
+const SPAN_DAYS = document.querySelector('span#days');
+const SPAN_HOURS = document.querySelector('span#hours');
+const SPAN_MINUTES = document.querySelector('span#minutes');
+const SPAN_SECONDS = document.querySelector('span#seconds');
+// Milliseconds for the calculations
+const MILLISECONDS_OF_A_SECOND = 1000;
+const MILLISECONDS_OF_A_MINUTE = MILLISECONDS_OF_A_SECOND * 60;
+const MILLISECONDS_OF_A_HOUR = MILLISECONDS_OF_A_MINUTE * 60;
+const MILLISECONDS_OF_A_DAY = MILLISECONDS_OF_A_HOUR * 24
+
+//===
+// FUNCTIONS
+//===
+
+/**
+ * Method that updates the countdown and the sample
+ */
+function updateCountdown() {
+    // Calcs
+    const NOW = new Date()
+    const DURATION = DATE_TARGET - NOW;
+    const REMAINING_DAYS = Math.floor(DURATION / MILLISECONDS_OF_A_DAY);
+    const REMAINING_HOURS = Math.floor((DURATION % MILLISECONDS_OF_A_DAY) / MILLISECONDS_OF_A_HOUR);
+    const REMAINING_MINUTES = Math.floor((DURATION % MILLISECONDS_OF_A_HOUR) / MILLISECONDS_OF_A_MINUTE);
+    const REMAINING_SECONDS = Math.floor((DURATION % MILLISECONDS_OF_A_MINUTE) / MILLISECONDS_OF_A_SECOND);
+
+    // Render
+    SPAN_DAYS.textContent = REMAINING_DAYS;
+    SPAN_HOURS.textContent = REMAINING_HOURS;
+    SPAN_MINUTES.textContent = REMAINING_MINUTES;
+    SPAN_SECONDS.textContent = REMAINING_SECONDS;
+}
+
+//===
+// INIT
+//===
+updateCountdown();
+// Refresh every second
+setInterval(updateCountdown, MILLISECONDS_OF_A_SECOND);
